13fb40cf65c755132e26a4fa3ef666e19b106860
Brian Helsel <bhelsel@kumc.edu>
2026-09-14 14:35:42 -0500

    Remove files not used

--- src/tests/tests.js
+++
@@ -1,9 +0,0 @@
-import ageEquivalentCalculator from "../functions/ageEquivalentCalculator.js";
-
-const result = ageEquivalentCalculator({
-  verbal_knowledge: "13",
-  riddles: "15",
-  matrices: "17",
-});
-
-console.log(result);
